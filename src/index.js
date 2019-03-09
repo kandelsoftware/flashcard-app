37180573f3a4482ad0f4c1f1859b0485aeadaa05
saroj <kandelsoftware@gmail.com>
2019-03-09 18:00:39 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const store = createStore((state, action) => {
+  switch (action.type) {
+    case "Add_CARD":
+      let newCard = Object.assign({}, action.data, {
+        score: 1,
+        id: +new Date()
+      });
+      return Object.assign({}, state, {
+        cards: state.cards ? state.cards.concat([newCard]) : [newCard]
+      });
+    default:
+      return state || { cards: [] };
+  }
+});
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch({
+  type: "Add_CARD",
+  data: {
+    front: "front",
+    back: "back"
+  }
+});
+export default store;
